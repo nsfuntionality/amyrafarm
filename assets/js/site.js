5819818873f8cbcbd0c8a77b4e17e4fb256fc0d6
nsfuntionality <46289143+nsfuntionality@users.noreply.github.com>
2022-03-18 10:12:28 -0500

    Add files via upload

--- assets/js/site.js
+++ assets/js/site.js
@@ -41,3 +41,27 @@
   }
 
 })(); // End of use strict
+
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true
+  },
+  initialSlide: 2,
+  pagination: {
+    el: ".swiper-pagination"
+  },
+  autoplay: {
+      delay: 2000,
+  },
+  mousewheel: {
+    invert: true,
+  },
+});
